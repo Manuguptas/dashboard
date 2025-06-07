1f45e7089d60821e61f0ee4866c4a1d15106878a
Manu gupta <144513877+Manuguptas@users.noreply.github.com>
2025-06-07 17:35:28 +0530

    Update db.php

--- db.php
+++ db.php
@@ -4,3 +4,19 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
+<!-- 
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profilePic` varchar(255) NOT NULL,
+  `signup_date` date NOT NULL,
+  `user_closed` varchar(3) NOT NULL,
+  `num_posts` int(11) NOT NULL,
+  `phoneNumber` int(50) NOT NULL,
+  `bio` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1; -->
